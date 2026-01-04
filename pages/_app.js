9efa42ffd86a2053540c06d3327a3c8ebb4e1b85
Parichehr <alfa@Caroline-Adkins.local>
2026-01-04 06:13:02 -0800

    fix : fixing query bug

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,27 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Layout from "@/Components/Layout/Layout";
 import "@/styles/globals.css";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />;
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+
+      {process.env.NODE_ENV === "development" && (
+        <ReactQueryDevtools initialIsOpen={false} />
+      )}
+    </QueryClientProvider>
   );
 }
