85a4cbeb94cbdaddcebadad67b6285de87a1c416
Parichehr <alfa@Caroline-Adkins.local>
2026-01-04 04:12:10 -0800

    feat : adding logo and title for the website

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="fr" dir="rtl">
-      <Head />
+      <Head>
+        <link rel="icon" href="/image/cover.svg" />
+        <title>Torino</title>
+      </Head>
+      <body></body>
       <body>
         <Main />
         <NextScript />
