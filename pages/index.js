8243354ee530f22b859fc6b8d23d8a1d215b9d07
Parichehr <alfa@Caroline-Adkins.local>
2026-01-04 07:39:43 -0800

    feat : adding home page and then import it into main index of page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import MainPage from "@/Components/Template/Home/MainPage";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <>
+      <MainPage />
+    </>
+  );
 }
 
-export default Home
+export default Home;
