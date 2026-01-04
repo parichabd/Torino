39ee51022732ee1d2d86fb2c826d8bf49df3170c
Parichehr <alfa@Caroline-Adkins.local>
2026-01-04 05:58:08 -0800

    fix bugs

---
+++ Hooks/useTours.js
@@ -0,0 +1,25 @@
+// import { useQuery } from "@tanstack/react-query";
+// import api from "@/services/api";
+
+// export default function useTours() {
+//   return useQuery({
+//     queryKey: ["tours"],
+//     queryFn: () => api.get("/tour").then((res) => res.data),
+//   });
+// }
+
+
+import { useQuery } from "@tanstack/react-query";
+import api from "@/Services/api";
+
+const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+
+export default function useTours() {
+  return useQuery({
+    queryKey: ["tours"],
+    queryFn: async () => {
+      await sleep(2000); // 👈 اینجاست (لودینگ)
+      return api.get("/tour").then((res) => res.data);
+    },
+  });
+}
